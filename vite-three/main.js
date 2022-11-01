2727eb20c7f1399e09762dddcc9124e542391da1
risingcore <vcjsm2283@gmail.com>
2022-11-01 23:13:49 +0900

    rectangle

---
+++ vite-three/main.js
@@ -0,0 +1,41 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+
+console.log(scene);
+console.log(camera);
+console.log(renderer);
+
+renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(devicePixelRatio);
+document.body.appendChild(renderer.domElement);
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+const mesh = new THREE.Mesh(boxGeometry, material);
+
+console.log(boxGeometry);
+console.log(material);
+
+scene.add(mesh);
+camera.position.z = 5;
+
+const planeGeometry = new THREE.PlaneGeometry(2, 2, 10, 10);
+const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(planeMesh);
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.01;
+  planeMesh.rotation.x += 0.01;
+}
+
+animate();
