718a2531b7d6c927a9ea2a4d3df9706854887069
risingcore <vcjsm2283@gmail.com>
2022-11-14 22:43:42 +0900

    position Attribute

--- course/09.geometries/src/script.js
+++ course/09.geometries/src/script.js
@@ -12,8 +12,21 @@ const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+const count = 5000;
+const positionArray = new Float32Array(count * 3 * 3);
+
+for (let i = 0; i < count * 3 * 3; i++) {
+  positionArray[i] = Math.random() - 0.5 * 4;
+}
+
+const positionAttribute = new THREE.BufferAttribute(positionArray, 3);
+
+const geometry = new THREE.BufferGeometry();
+geometry.setAttribute('position', positionAttribute);
+
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
